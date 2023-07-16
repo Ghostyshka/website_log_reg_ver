565ff06b22f6ac14de80625fe5c56843207d21a1
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2023-07-16 18:43:57 +0300

    add User Registration

---
+++ demo/src/main/java/com/example/demo/appuser/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.demo.AppUser.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "all works correctly";
+    }
+}
