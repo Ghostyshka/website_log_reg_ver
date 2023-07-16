904ea6d7108f95ddca4145fce3bd05f3cc4e0211
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2023-07-17 00:01:45 +0300

    innit Spring Security

---
+++ demo/src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.security.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public final class WebSecurityConfig extends WebSecurityConfigurerApapter {
+
+
+}
